2e375a6fc271ca842bfb8307979934724f1c0ff9
JavierEDS <javier.diaz@jala.university>
2024-04-12 12:07:57 -0500

    Validaciones de tareas

--- src/main/java/com/codecrafters/todolistbackend/domain/tasks/TaskService.java
+++ src/main/java/com/codecrafters/todolistbackend/domain/tasks/TaskService.java
@@ -1,6 +1,7 @@
 package com.codecrafters.todolistbackend.domain.tasks;
 
 import com.codecrafters.todolistbackend.db.collections.fields.TaskFields;
+import com.codecrafters.todolistbackend.domain.validations.TodoValidator;
 import com.codecrafters.todolistbackend.exceptions.UserDoesNotExistsException;
 import java.time.LocalDate;
 import java.util.List;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Service;
 class TaskService {
 
   private final TaskRepository taskRepository;
+  private final TodoValidator todoValidator;
 
-  TaskService(TaskRepository taskRepository, ApplicationEventMulticaster eventPublisher) {
+  TaskService(TaskRepository taskRepository, ApplicationEventMulticaster eventPublisher, TodoValidator todoValidator) {
     this.taskRepository = taskRepository;
+    this.todoValidator = todoValidator;
   }
 
   void addUserTask(ObjectId userID, TaskCreationDTO task) throws UserDoesNotExistsException {
@@ -28,12 +31,12 @@ class TaskService {
       throw new InvalidTitleException(task.title());
     }
 
-
+    if (!todoValidator.userHasCategory(userID, task.category())) {
+      throw new InvalidTaskCategory(task.category());
+    }
 
     taskRepository.createUserTask(userID, task);
   }
-
-
 
   void deleteUserTask(ObjectId userID, ObjectId taskID) throws UserDoesNotExistsException {
     taskRepository.deleteUserTask(userID, taskID);
@@ -41,6 +44,38 @@ class TaskService {
 
   void completeUserTask(ObjectId userID, ObjectId taskID) throws UserDoesNotExistsException {
     taskRepository.updateField(userID, taskID, TaskFields.COMPLETED, Boolean.TRUE);
+  }
+
+  void updateTitleUserTask(ObjectId userID, ObjectId taskID, String title) throws UserDoesNotExistsException {
+
+    if (title.isEmpty()) {
+      throw new InvalidTitleException(title);
+    }
+
+    taskRepository.updateField(userID, taskID, TaskFields.TITLE, title);
+  }
+
+  void updateDescriptionUserTask(ObjectId userID, ObjectId taskID, String description) throws UserDoesNotExistsException {
+    taskRepository.updateField(userID, taskID, TaskFields.DESCRIPTION, description);
+  }
+
+  void updateDueDateUserTask(ObjectId userID, ObjectId taskID, String dueDate) throws UserDoesNotExistsException {
+
+    if (!dueDate.matches(TaskValidationRegex.DATE_VALIDATION)
+        || LocalDate.parse(dueDate).isBefore(LocalDate.now())) {
+      throw new InvalidDateException(dueDate);
+    }
+
+    taskRepository.updateField(userID, taskID, TaskFields.DUE_DATE, dueDate);
+  }
+
+  void updateCategoryUserTask(ObjectId userID, ObjectId taskID, String category) throws UserDoesNotExistsException {
+
+    if (!todoValidator.userHasCategory(userID, category)) {
+      throw new InvalidTaskCategory(category);
+    }
+
+    taskRepository.updateField(userID, taskID, TaskFields.DUE_DATE, category);
   }
 
   List<Task> getAllUserTasks(ObjectId userID) throws UserDoesNotExistsException {

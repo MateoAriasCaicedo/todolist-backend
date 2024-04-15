7340ee443678c624a7bb67c2107688a047442348
Luis Mateo Arias Caicedo <mateo.arias@jala.university>
2024-04-15 12:10:13 -0500

    Solve database credentials issue

--- src/main/java/com/codecrafters/todolistbackend/database/DBConfig.java
+++ src/main/java/com/codecrafters/todolistbackend/database/DBConfig.java
@@ -2,15 +2,19 @@ package com.codecrafters.todolistbackend.database;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DBConfig {
 
+  @Value("${spring.data.mongodb.uri}")
+  private String dbUrl;
+
   /** The database client for the todoapp database. */
   @Bean(name = "todoMongoClient")
   public MongoClient mongoClient() {
-    return MongoClients.create("todo");
+    return MongoClients.create(dbUrl);
   }
 }
